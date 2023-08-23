9d865bf0242bbafa8bc3743e30738f6ee7561d49
nyomsas99 <nyomsas1234@gmail.com>
2023-08-23 21:55:59 +0700

    bsbns

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-// countdown
-// simplyCountdown(".simply-countdown", {
-//   year: 2023, // required
-//   month: 9, // required
-//   day: 2, // required
-//   hours: 8, // Default is 0 [0-23] integer
-
-//   words: {
-//     //words displayed into the countdown
-//     days: { singular: "hari", plural: "hari" },
-//     hours: { singular: "jam", plural: "jam" },
-//     minutes: { singular: "menit", plural: "menit" },
-//     seconds: { singular: "detik", plural: "detik" },
-//   },
-// });
 // toggler navbar
 const hamburger = document.querySelector(".navbar-toggler");
 const stickyTop = document.querySelector(".sticky-top");
