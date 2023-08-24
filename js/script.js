a1ef546604c9d0e1460945248f75e66fad6e1e85
nyomsas99 <nyomsas1234@gmail.com>
2023-08-24 12:24:20 +0700

    bsbwb

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ hamburger.addEventListener("click", function () {
 offcanvas.addEventListener("hidden.bs.offcanvas", function () {
   stickyTop.style.overflow = "hidden";
 });
+
+// my social media toggle
+const floating_btn = document.querySelector(".floating-btn");
+const close_btn = document.querySelector(".close-btn");
+const social_panel_container = document.querySelector(
+  ".social-panel-container"
+);
+
+floating_btn.addEventListener("click", () => {
+  social_panel_container.classList.toggle("visible");
+});
+
+close_btn.addEventListener("click", () => {
+  social_panel_container.classList.remove("visible");
+});
+
+const abt = document.querySelector(".abt");
+
+abt.addEventListener("mouseenter", () => {
+  abt.style.backgroundColor = "rgb(100, 50, 120)";
+});
